f24d812813a2891319cc05e8c11385c899b1f0a3
Max Hoffman <maximilian.wolfgang1@gmail.com>
2017-06-09 14:08:57 -0700

    All of the endpoints work correctly, i think 404's are 500's are handled approptiately

--- server/controllers/notes.js
+++ server/controllers/notes.js
@@ -2,46 +2,82 @@ const path = require('path');
 const Promise = require('bluebird');
 const db = require('../models');
 
-//TODO: Get note endpoint
-exports.getOneNoteCtrl = function(req, res) {
-  res.send({"hit get note endpoint" : true})
-  return new Promise(function(resolve, reject) {
-    
-  });
-};
+module.exports = {
+  getOneNoteCtrl: function(req, res) {
+    if (!req.params.id) {
+      res.sendStatus(404, { 'error': 'Specify note id and try again.' });
+      return;
+    }
+    db.Note.findOne({ where: { id: req.params.id }})
+    .then(note => {
+      res.send(note);
+    })
+    .catch(error => {
+      console.log('get error in server:', error);
+      res.sendStatus(505, error);
+    });
+  },
 
-//TODO: Post note endpoint
-exports.postNoteCtrl = function(req, res) {
-  db.Note.create({ title: req.body.title, text: req.body.text })
-  .then(note => {
-    console.log('post success:', note.dataValues.title);
-    res.send(note);
-  })
-  .catch(error => {
-    console.log('post error:', error);
-  })
-};
+  postNoteCtrl: function(req, res) {
+    if (!req.body.title || !req.body.text) {
+      res.sendStatus(404, { 'error': 'Tried to submit with empty fields.' });
+      return;
+    }
+    db.Note.create({ title: req.body.title, text: req.body.text })
+    .then(note => {
+      console.log('post success:', note.dataValues.title);
+      res.send(note);
+    })
+    .catch(error => {
+      console.log('post error in server:', error);
+      res.sendStatus(505, error);
+    });
+  },
 
-//TODO: Get all notes endpoint
-exports.getNotesCtrl = function(req, res) {
-  db.Note.findAll()
-  .then(notes => {
-    res.send(notes);
-  })
-};
+  getNotesCtrl: function(req, res) {
+    db.Note.findAll()
+    .then(notes => {
+      res.send(notes);
+    })
+    .catch(error => {
+      console.log('get error in server:', error);
+      res.sendStatus(500, error);
+    });
+  },
 
-//TODO: Update note endpoint
-exports.updateNoteCtrl = function(req, res) {
-  res.send({"hit update endpoint" : true})
-  return new Promise(function(resolve, reject) {
+  updateNoteCtrl: function(req, res) {
+    if (!req.params.id || !req.body.title || !req.body.text) {
+      res.sendStatus(404, { 'error': 'Specify note id, title, text and try again.' });
+      return;
+    }
+    db.Note
+    .update({ 
+      title: req.body.title,
+      text: req.body.text 
+    }, {
+      where: { id: req.params.id }             
+    })
+    .then(indexes => {
+      res.send({'success': `updated notes in database indexes: ${indexes}`});
+    })
+    .catch(error => {
+      console.log('update error in server:', error);
+      res.sendStatus(500, error);
+    });
+  },
 
-  });
-};
-
-//TODO: Delete note endpoint
-exports.deleteNoteCtrl = function(req, res) {
-  res.send({"hit delete endpoint" : true})
-  return new Promise(function(resolve, reject) {
-
-  });
-};
+  deleteNoteCtrl: function(req, res) {
+    if (!req.params.id) {
+      res.sendStatus(404, { 'error': 'Specify note id and try again.' });
+      return;
+    }
+    db.Note.destroy({ where: { id: req.params.id }})
+    .then(() => {
+      res.send({ 'success': `deleted note with id = + ${req.params.id}` });
+    })
+    .catch(error => {
+      console.log('delete error in server:', error);
+      res.sendStatus(500, error);
+    });
+  }
+}
